9daa5b72a3b1b8317af276308d3cb288294b26c0
Gabriel Buzo <102891149+Buzonga@users.noreply.github.com>
2024-08-01 15:28:57 -0300

    Finalizado o projeto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,66 @@
 function sortear() {
-    let quantidade = parseInt(document.getElementById("quantidade").value);
-    let de = parseInt(document.getElementById("de").value);
-    let ate = parseInt(document.getElementById("ate").value);
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
     let sorteados = [];
     let numero;
 
-    for (let i = 0; i < quantidade; i++) {
+    if (de >= ate) {
+        alert(`O número mínimo é ${de} que é maior que o número máximo ${ate}. Insira um número mínimo menor que o número máximo.`);
+        document.getElementById('quantidade').value = '';
+        document.getElementById('de').value = '';
+        document.getElementById('ate').value = '';
         numero = obterNumeroAleatorio(de, ate);
-        sorteados.push(numero)
+        return;
     }
 
-    alert(sorteados);
+    if ((ate - de + 1) < quantidade) {
+        alert(`Você está pedindo para sortear mais números que os números possíveis entre ${de} e ${ate}. Insira uma quantia menor de números ou aumente o número máximo.`);
+        document.getElementById('quantidade').value = '';
+        document.getElementById('de').value = '';
+        document.getElementById('ate').value = '';
+        numero = obterNumeroAleatorio(de, ate);
+        return;
+    }
 
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+            document.getElementById('de').value = '';
+            document.getElementById('ate').value = '';
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+    alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado') ) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
 }
